0b1c05aa3feb4cc1bb119e78d3482b9ea991c0a4
Crispo000 <2ghsc8qjdt@privaterelay.appleid.com>
2026-02-03 11:56:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Sito CDC caricato correttamente");
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
